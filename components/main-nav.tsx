e364a47ab32cad416f27255052b2dd0be9b5001a
Anshuman Rai <118046194+anshuman-rai-27@users.noreply.github.com>
2024-02-14 03:15:30 +0530

    categories page created

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -18,6 +18,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}`,
         },
         {
+            href:`/${params.storeId}/categories`,
+            label: `Categories`,
+            active: pathname === `/${params.storeId}/categories`,
+        },
+        {
             href:`/${params.storeId}/billboards`,
             label: `Billboards`,
             active: pathname === `/${params.storeId}/billboards`,
