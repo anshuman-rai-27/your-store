a67d16d7bdb6d6bcb79efa7481b87fe367b4c2a6
Anshuman Rai <118046194+anshuman-rai-27@users.noreply.github.com>
2024-02-15 22:00:34 +0530

    size component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -23,6 +23,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}/categories`,
         },
         {
+            href:`/${params.storeId}/sizes`,
+            label: `Sizes`,
+            active: pathname === `/${params.storeId}/sizes`,
+        },
+        {
             href:`/${params.storeId}/billboards`,
             label: `Billboards`,
             active: pathname === `/${params.storeId}/billboards`,
